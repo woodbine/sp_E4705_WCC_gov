658e0f6123be22362dc05bf42ef37b41dd94bbb1
blablupcom <o.volodin@yahoo.com>
2015-08-28 16:12:43 +0300

    added lxml to bs

--- scraper.py
+++ scraper.py
@@ -57,7 +57,7 @@ def convert_mth_strings ( mth_string ):
     return mth_string
 # pull down the content from the webpage
 html = urllib2.urlopen(url)
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, 'lxml')
 # find all entries with the required class
 block = soup.find('div', attrs = {'id':'ctl00_PlaceHolderMain_ctl00_ctl00_page_container'})
 links = block.findAll('a', href=True)
