080542a7ea81fd81cb2103894b52743ec2d93865
blablupcom <olegvolodin@eyahoo.com>
2015-09-24 21:59:01 +0300

    first commit

--- scraper.py
+++ scraper.py
@@ -1,42 +1,49 @@
 # -*- coding: utf-8 -*-
+
+#### IMPORTS 1.0
+
 import os
 import re
-import requests
 import scraperwiki
 import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
-from dateutil.parser import parse
+import requests
 import urllib
 
-# Set up variables
-entity_id = "E4705_WCC_gov"
-url = "http://www.wakefield.gov.uk/about-the-council/budget-and-spending/payments-to-suppliers"
-errors = 0
-# Set up functions
+#### FUNCTIONS 1.0
+
 def validateFilename(filename):
-    filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9][0-9]$'
-    dateregex = '[0-9][0-9][0-9][0-9]_[0-9][0-9]'
+    filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
+    dateregex = '[0-9][0-9][0-9][0-9]_[0-9QY][0-9]'
     validName = (re.search(filenameregex, filename) != None)
     found = re.search(dateregex, filename)
     if not found:
         return False
     date = found.group(0)
-    year, month = int(date[:4]), int(date[5:7])
     now = datetime.now()
-    validYear = (2000 <= year <= now.year)
-    validMonth = (1 <= month <= 12)
+    year, month = date[:4], date[5:7]
+    validYear = (2000 <= int(year) <= now.year)
+    if 'Q' in date:
+        validMonth = (month in ['Q0', 'Q1', 'Q2', 'Q3', 'Q4'])
+    elif 'Y' in date:
+        validMonth = (month in ['Y1'])
+    else:
+        try:
+            validMonth = datetime.strptime(date, "%Y_%m") < now
+        except:
+            return False
     if all([validName, validYear, validMonth]):
         return True
+
+
 def validateURL(url):
     try:
-        #r = requests.get(url, allow_redirects=True, timeout=20)
         r = urllib.urlopen(url)
         count = 1
         while r.getcode() == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            #r = requests.get(url, allow_redirects=True, timeout=20)
             r = urllib.urlopen(url)
         sourceFilename = r.headers.get('Content-Disposition')
 
@@ -48,62 +55,90 @@ def validateURL(url):
         validFiletype = ext in ['.csv', '.xls', '.xlsx']
         return validURL, validFiletype
     except:
-        raise
+        print ("Error validating URL.")
+        return False, False
+
+
+def validate(filename, file_url):
+    validFilename = validateFilename(filename)
+    validURL, validFiletype = validateURL(file_url)
+    if not validFilename:
+        print filename, "*Error: Invalid filename*"
+        print file_url
+        return False
+    if not validURL:
+        print filename, "*Error: Invalid URL*"
+        print file_url
+        return False
+    if not validFiletype:
+        print filename, "*Error: Invalid filetype*"
+        print file_url
+        return False
+    return True
+
+
 def convert_mth_strings ( mth_string ):
-
     month_numbers = {'JAN': '01', 'FEB': '02', 'MAR':'03', 'APR':'04', 'MAY':'05', 'JUN':'06', 'JUL':'07', 'AUG':'08', 'SEP':'09','OCT':'10','NOV':'11','DEC':'12' }
-    #loop through the months in our dictionary
     for k, v in month_numbers.items():
-#then replace the word with the number
-
         mth_string = mth_string.replace(k, v)
     return mth_string
-# pull down the content from the webpage
+
+
+#### VARIABLES 1.0
+
+entity_id = "E4705_WCC_gov"
+url = "http://www.wakefield.gov.uk/about-the-council/budget-and-spending/payments-to-suppliers"
+errors = 0
+data = []
+
+#### READ HTML 1.1 - no "lxml"
+
 html = urllib2.urlopen(url)
-soup = BeautifulSoup(html, 'lxml')
-# find all entries with the required class
+soup = BeautifulSoup(html, "lxml")
+
+#### SCRAPE DATA
+
 block = soup.find('div', attrs = {'id':'ctl00_PlaceHolderMain_ctl00_ctl00_page_container'})
 links = block.findAll('a', href=True)
 for link in links:
     csvfile = link.text.strip()
-    linkfile = 'http://www.wakefield.gov.uk' + link['href']
-    if not '.pdf' in linkfile:
+    url = 'http://www.wakefield.gov.uk' + link['href']
+    if not '.pdf' in url:
        # csv_year = csvfile.split('-')[-1].strip().split(' ')[0]
         csv_year = csvfile.split('-')[-2].strip()
         # if '16 Quarter 1' in csvfile.split('-')[-1]:
         #     csv_year = '15'
         if 'Quarter 4' in csvfile:
-            csvMth = 'Dec'
+            csvMth = 'Q4'
         if 'Quarter 3' in csvfile:
-            csvMth = 'Sep'
+            csvMth = 'Q3'
         if 'Quarter 2' in csvfile:
-            csvMth = 'Jun'
+            csvMth = 'Q2'
         if 'Quarter 1' in csvfile:
-            csvMth = 'Mar'
+            csvMth = 'Q1'
         csvYr = csv_year
         csvMth = convert_mth_strings(csvMth.upper())
-        if 'Quarter' in link.text:
-            filename = 'Q'+entity_id + "_" + csvYr + "_" + csvMth
         todays_date = str(datetime.now())
-        file_url = linkfile.strip()
-        validFilename = validateFilename(filename)
-        validURL, validFiletype = validateURL(file_url)
-        if not validFilename:
-            print filename, "*Error: Invalid filename*"
-            print file_url
-            errors += 1
-            continue
-        if not validURL:
-            print filename, "*Error: Invalid URL*"
-            print file_url
-            errors += 1
-            continue
-        if not validFiletype:
-            print filename, "*Error: Invalid filetype*"
-            print file_url
-            errors += 1
-            continue
+        data.append([csvYr, csvMth, url])
+
+#### STORE DATA 1.0
+
+for row in data:
+    csvYr, csvMth, url = row
+    filename = entity_id + "_" + csvYr + "_" + csvMth
+    todays_date = str(datetime.now())
+    file_url = url.strip()
+
+    valid = validate(filename, file_url)
+
+    if valid == True:
         scraperwiki.sqlite.save(unique_keys=['l'], data={"l": file_url, "f": filename, "d": todays_date })
         print filename
+    else:
+        errors += 1
+
 if errors > 0:
     raise Exception("%d errors occurred during scrape." % errors)
+
+#### EOF
+
